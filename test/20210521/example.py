af0a4cfdf5cca9de9ceb7961bebf19571ad2ab2f
NU <a175@users.noreply.github.com>
2021-05-21 12:50:14 +0900

    add

--- test/20210521/example.py
+++ test/20210521/example.py
@@ -11,3 +11,15 @@ class SquareToCircle(manim.scene.scene.Scene):
         self.play(manim.animation.creation.Create(square))
         self.play(manim.animation.transform.Transform(square, circle))
         self.play(manim.animation.fading.FadeOut(square))
+
+
+class PutSquareToCircle(manim.scene.scene.Scene):
+    def construct(self):
+        circle = manim.mobject.geometry.Circle()
+        square = manim.mobject.geometry.Square()
+        square.flip(manim.constants.RIGHT)
+        square.rotate(-3 * manim.constants.TAU / 8)
+        circle.set_fill(manim.utils.color.PINK, opacity=0.5)
+        self.add(square)
+        self.play(manim.animation.transform.Transform(square, circle))
+        self.play(manim.animation.fading.FadeOut(square))

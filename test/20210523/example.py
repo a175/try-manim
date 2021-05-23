0a03b78aee94de416516ffdf4a71913e7d0171d9
NU <a175@users.noreply.github.com>
2021-05-23 14:28:55 +0900

    color

--- test/20210523/example.py
+++ test/20210523/example.py
@@ -2,28 +2,48 @@ import manim
 
 class MyScene(manim.scene.scene.Scene):
     def construct(self):
-
-        vg=self.magic_circle(3,2)        
+        vg=CircleWithPolygons(3,2)
+        #vg=self.magic_circle(3,2)        
         self.wait()
         self.play(manim.animation.creation.Create(vg))
-        vg=self.magic_circle(4,4)        
-        vg.scale(1.5)
-        self.wait()
+        #vg=self.magic_circle(4,4)        
+        vg=CircleWithPolygons(4,3,is_variant=True)
+        vg.scale(0.8)
+        #self.wait()
         self.play(manim.animation.creation.Create(vg))
         self.wait()
 
 
-    def magic_circle(self,n,m):
-        delta = -2.0*manim.constants.PI/n/m
+class CircleWithPolygons(manim.mobject.types.vectorized_mobject.VGroup):
+    def __init__(self,n,m,is_variant=False):
+        delta = 2.0*manim.constants.PI/n/m
         all_obj = []
+
+        default_color = '#CCCCCC' 
+        colors = [default_color for i in range(m)] + [default_color]
         
-        circle = manim.mobject.geometry.Circle()
+        circle = manim.mobject.geometry.Circle(color=colors[-1])
         all_obj.append(circle)
         
         polygon0 = manim.mobject.geometry.RegularPolygon(n,0.5*manim.constants.PI)
         for i in range(m):
+            if i == 0 and is_variant:
+                line=manim.mobject.geometry.Line(manim.constants.LEFT,manim.constants.ORIGIN,color=colors[i])
+                line.rotate_about_origin(-(-1.0/n+1.0)*manim.constants.PI)
+                line.scale_about_point(0.1,manim.constants.ORIGIN)
+                line.shift(manim.constants.UP)
+                all_obj.append(line)
+                
+            polygon0.set_color(colors[i])
             all_obj.append(polygon0)
             polygon0=polygon0.copy()
-            polygon0.rotate_about_origin(delta)
-        mc = manim.mobject.types.vectorized_mobject.VGroup(*all_obj)
-        return mc
+            polygon0.rotate_about_origin(-delta)
+            
+            if i == 0 and is_variant:
+                line=manim.mobject.geometry.Line(manim.constants.ORIGIN,manim.constants.RIGHT,color=colors[i])
+                line.rotate_about_origin((-1.0/n+1.0)*manim.constants.PI)
+                line.scale_about_point(0.1,manim.constants.ORIGIN)
+                line.shift(manim.constants.UP)
+                all_obj.append(line)
+        super().__init__(*all_obj)
+

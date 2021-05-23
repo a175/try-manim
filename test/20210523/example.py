aebe58360aafc4191980323e4d602c0f21892c9f
NU <a175@users.noreply.github.com>
2021-05-23 13:08:21 +0900

    vgroup

--- test/20210523/example.py
+++ test/20210523/example.py
@@ -2,17 +2,19 @@ import manim
 
 class MyScene(manim.scene.scene.Scene):
     def construct(self):
+        
         circle = manim.mobject.geometry.Circle()
         polygon1 = manim.mobject.geometry.RegularPolygon(4,0.5*manim.constants.PI)
         polygon2 = polygon1.copy()
         polygon2.rotate(0.25*manim.constants.PI)
-        vg = manim.mobject.types.vectorized_mobject.VGroup(circle,polygon1,polygon2)
+        listofobj = [circle,polygon1,polygon2]
+        vg = manim.mobject.types.vectorized_mobject.VGroup(*listofobj)
         self.wait()
-        fadeinanimation=manim.animation.fading.FadeIn(vg)
-        self.play(fadeinanimation)
-        self.wait()
-        #self.play(manim.animation.creation.Create(polygon))
+        self.play(manim.animation.creation.Create(vg))
+        #fadeinanimation=manim.animation.fading.FadeIn(vg)
+        #self.play(fadeinanimation)
         self.wait()
 
 
 
+    

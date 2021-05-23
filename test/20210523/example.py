980cfe4a07afddb07543c31656bc33b564285380
NU <a175@users.noreply.github.com>
2021-05-23 12:55:14 +0900

    fadein

--- test/20210523/example.py
+++ test/20210523/example.py
@@ -3,16 +3,15 @@ import manim
 class MyScene(manim.scene.scene.Scene):
     def construct(self):
         circle = manim.mobject.geometry.Circle()
-        polygon = manim.mobject.geometry.RegularPolygon(4,0.5*manim.constants.PI)
+        polygon1 = manim.mobject.geometry.RegularPolygon(4,0.5*manim.constants.PI)
+        polygon2 = polygon1.copy()
+        polygon2.rotate(0.25*manim.constants.PI)
+        vg = VGroup(circle,polygon1,polygon2)
         self.wait()
-        fadeinanimation=manim.animation.fading.FadeIn(circle)
+        fadeinanimation=manim.animation.fading.FadeIn(vg)
         self.play(fadeinanimation)
         self.wait()
-        self.add(polygon)
-        self.wait()
-        polygon=polygon.copy()
-        polygon.rotate(0.25*manim.constants.PI)
-        self.play(manim.animation.creation.Create(polygon))
+        #self.play(manim.animation.creation.Create(polygon))
         self.wait()
 
 
